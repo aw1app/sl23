37422ddc14b34c9554fd288dd62cae121bd409c6
Your Name <you@1.com>
2023-10-01 22:38:04 +0530

    product component ui update

---
+++ ANGULAR/my-angular-app/src/app/user/user.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent {
+
+  userName='Satish';
+  email= 'sat@gmail.com'
+
+
+}
