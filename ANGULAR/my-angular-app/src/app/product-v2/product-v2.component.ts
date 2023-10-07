caee35a1379b2c11df2c3c7f628c802e5854847f
Your Name <you@1.com>
2023-10-07 22:09:30 +0530

    parent child communication

--- ANGULAR/my-angular-app/src/app/product-v2/product-v2.component.ts
+++ ANGULAR/my-angular-app/src/app/product-v2/product-v2.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Product } from '../model/product';
 
 @Component({
@@ -9,4 +9,18 @@ import { Product } from '../model/product';
 export class ProductV2Component {
 
   product:Product = new Product("Nokia", 60000,"Nokia latest Angroid phone v13.0", "assets/images/NokiaPhone.png");
+
+  //Demo getting data from the parent component
+@Input('var1') messageFromParent!:string;
+
+
+  str1:string="some string";
+
+  f2=()=> console.log(this.messageFromParent);
+
+  i:number=0;
+  f=():void=> {
+    console.log(`Called f() .. ${this.i++} times`);
+    console.log(`str1  is  ${this.str1} `);
+  }
 }
